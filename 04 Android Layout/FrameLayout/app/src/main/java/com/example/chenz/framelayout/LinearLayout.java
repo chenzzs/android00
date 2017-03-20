beef5124c8b2ada754ff07c74ef7e5e2a50d8421
chenzzs <975367319@qq.com>
2017-03-20 16:04:03 +0800

    Layout

---
+++ 04 Android Layout/FrameLayout/app/src/main/java/com/example/chenz/framelayout/LinearLayout.java
@@ -0,0 +1,16 @@
+package com.example.chenz.framelayout;
+
+import android.os.Bundle;
+
+/**
+ * Created by chenz on 2017/3/20.
+ */
+
+public class LinearLayout extends MainActivity{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
